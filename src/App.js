f4ef2779dc9fcf336d34ba5489ea65c9bb2a3e5a
jadambhawna9098 <bhawnajadam9@gmail.com>
2023-04-11 23:35:34 +0530

    studentapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import data from './components/data';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      studentList:data,
+      branchFilter: "all",
+      rollStatus: false
+    }
+  }
+  addStudent = ()=>{
+    let name = this.name.value;
+    let mobile = this.mobile.value;
+    let roll = this.roll.value;
+    let branch = this.branch.value;
+
+    let newStudent = {name,mobile,roll,branch};
+    this.setState({
+      studentList: [...this.state.studentList,newStudent] // 
+    })
+  }
+  changeBranchFilter = (branch)=>{
+    this.setState({
+      branchFilter: branch
+    })
+  }
+  validateRoll = ()=>{
+    let roll = this.roll.value;
+    let status = this.state.studentList.some((student)=>student.roll == roll);
+    this.setState({rollStatus: status});
+  }
+  deleteRecord = (rollNumber)=>{
+    if(window.confirm("Are you sure ?")){
+      let index = this.state.studentList.findIndex((student)=>student.roll == rollNumber);
+      this.state.studentList.splice(index,1);
+      this.setState({studentList: this.state.studentList});
+    }
+  }
+  render(){
+     return <div className="container">
+      <div className="row bg-primary mt-2">
+         <div className="col-12 text-center p-2 text-white font-weight-bold">Student App</div>
+      </div>
+      <div className="row mt-5 mb-5">
+        <div className="col-5 ">
+          <input style={{'borderRadius':'10px','boxShadow':'1px 1px 1px' , width: '60%'}} onBlur={this.validateRoll} ref={(roll)=>this.roll = roll} type="text" className="form-control" placeholder='Enter Roll Number'/>
+          {this.state.rollStatus && <small className='text-danger'>Roll number is already taken</small>}
+        </div>
+        <div className="col-5">
+          <input style={{'borderRadius':'10px','boxShadow':'1px 1px 1px' , width: '60%'}} ref={(name)=>this.name = name} type="text" className="form-control" placeholder='Enter Name'/>
+        </div>
+      </div>
+      <div className="row mt-5 mb-2">
+        <div className="col-5">
+          <input style={{'borderRadius':'10px','boxShadow':'1px 1px 1px' , width: '60%'}} ref={(mobile)=>this.mobile = mobile} type="text" className="form-control" placeholder='Enter Mobile Number'/>
+        </div>
+        <div className="col-5">
+          <select style={{'borderRadius':'10px','boxShadow':'1px 1px 1px' , width: '60%'}} ref={(branch)=>this.branch = branch} className="form-control">
+            <option value="0">Select Branch</option>
+            <option value="CS">CS</option>
+            <option value="IT">IT</option>
+            <option value="CV">CV</option>
+            <option value="EC">EC</option>
+          </select>
+        </div>
+      </div>
+      <div className="row mt-5 mb-2">
+        <div className="col-2">
+          <button onClick={this.addStudent} style={{width:'80%'}} className="btn btn-primary">Add</button>
+        </div>
+        <div className="col-5 offset-3 text-right">
+          <button onClick={()=>{this.changeBranchFilter("CS")}} className="btn btn-primary">CS ({this.state.studentList.filter(student=>student.branch=="CS").length})</button>
+          <button onClick={()=>{this.changeBranchFilter("IT")}} className="btn btn-danger ml-1">IT ({this.state.studentList.filter(student=>student.branch=="IT").length})</button>
+          <button onClick={()=>{this.changeBranchFilter("CV")}} className="btn btn-warning ml-1">CV ({this.state.studentList.filter(student=>student.branch=="CV").length})</button>
+          <button onClick={()=>{this.changeBranchFilter("EC")}} className="btn btn-success ml-1">EC ({this.state.studentList.filter(student=>student.branch=="EC").length})</button>
+          <button onClick={()=>{this.changeBranchFilter("all")}}className="btn btn-outline-success ml-1">Total: ({this.state.studentList.length})</button>
+        </div>
+      </div>
+      <hr/>
+      <table style={{boxShadow:"1px 2px 5px black"}} className="table">
+         <thead style={{boxShadow:"1px 1px 1px black"}}>
+            <tr>
+              <th>S.no</th>
+              <th>Roll</th>
+              <th>Name</th>
+              <th>Mobile</th>
+              <th>Branch</th>
+              <th>Delete</th>
+            </tr>
+         </thead>
+         <tbody>
+            {this.state.studentList.filter((student)=>this.state.branchFilter=="all"||student.branch == this.state.branchFilter).map((student,index)=><tr>
+              <td>{index+1}</td>
+              <td>{student.roll}</td>
+              <td>{student.name}</td>
+              <td>{student.mobile}</td>
+              <td>{student.branch}</td>
+              <td>
+                <button  onClick={()=>this.deleteRecord(student.roll)}  className="btn btn-outline-danger">Delete</button>
+              </td>
+            </tr>)}
+         </tbody>
+      </table>
+     </div>
+  }
 }
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        "roll": 1001,
+        "name": "bhawna",
+        "mobile":9090909090,
+        "branch": "CS"
+    },
+    {
+        "roll": 1002,
+        "name": "nikki",
+        "mobile":9098989898,
+        "branch": "CS"
+    },
+    {
+        "roll": 1003,
+        "name": "tanu",
+        "mobile":9797979797,
+        "branch": "IT"
+    },
+    {
+        "roll": 1004,
+        "name": "seetal",
+        "mobile":9696969696,
+        "branch": "EC"
+    },
+    {
+        "roll": 1005,
+        "name": "anjali",
+        "mobile":9595959595,
+        "branch": "CV"
+    },
+    {
+        "roll": 1006,
+        "name": "rinku",
+        "mobile":9494949494,
+        "branch": "IT"
+    }
+]
